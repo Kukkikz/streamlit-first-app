af80ea1d620aaa55bf52b63a1601e86af33530fa
Kukkikz <pranout_kik@hotmail.com>
2024-07-14 14:45:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import pandas as pd
+import altair as alt
 
-st.title('🎈 App Name')
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/code/027e657f961692e685aff038f73122e68dc9ad1c/streamlit/part3/penguins_cleaned.csv")
 
-st.write('Hello world!')
+c = (
+   alt.Chart(df)
+   .mark_circle()
+   .encode(x="bill_length_mm", y="body_mass_g", color="species")
+)
+
+st.title("🦆 My Scatter Plot")
+st.altair_chart(c, use_container_width=True)
